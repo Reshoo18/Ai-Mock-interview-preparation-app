9c98df696ce2a222f520c593221d9713803a529a
Reshoo-18 <reshooranjan7@gmail.com>
2026-01-04 13:23:14 +0530

    add button

--- frontend/src/pages/Home/Dashboard.jsx
+++ frontend/src/pages/Home/Dashboard.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import {LuPlus} from "react-icons/lu";
+import { CARD_BG } from '../../utils/data';
+import toast from "react-hot-toast";
+import DashboardLayout from '../../component/layouts/DashboardLayout';
+import { useNavigate } from 'react-router-dom';
 const Dashboard = () => {
+    const navigate=useNavigate();
+    const [openCreateModal,setOpenCreateModal]=useState(false);
+    const [session,setSession]=useState([]);
+
+    const [openDeleteAlert,setOpenDeleteAlert]=useState({
+        open: false,
+        data: null,
+    });
+
+    const fetchAllSessions=async () =>{
+
+    };
+
+    const deleteSessions = async (sessionData) =>{};
+    useEffect(()=>{
+        fetchAllSessions();
+    },[])
+
   return (
     <div>
-      Dashboard
+      <DashboardLayout>
+        <div className=''>
+            <div className=''>
+
+            </div>
+            <button className='' onClick={()=>setOpenCreateModal(true)}>
+                <LuPlus className=''/>
+                Add New
+            </button>
+        </div>
+      </DashboardLayout>
     </div>
   )
 }
