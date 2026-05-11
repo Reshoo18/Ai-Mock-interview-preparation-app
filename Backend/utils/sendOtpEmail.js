29e818e0857ea726d06d038923e317f06390c518
Reshoo-18 <reshooranjan7@gmail.com>
2026-05-11 14:34:21 +0530

    edits

--- Backend/utils/sendOtpEmail.js
+++ Backend/utils/sendOtpEmail.js
@@ -1,30 +1,31 @@
 const nodemailer = require("nodemailer");
 
 const sendOtpEmail = async (email, otp) => {
+
   try {
 
     const transporter = nodemailer.createTransport({
 
       host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
 
-      requireTLS: true,
+      port: 465,
 
-      tls: {
-        family: 4,
-        rejectUnauthorized: false,
-      },
+      secure: true,
 
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
+
+      family: 4, // FORCE IPV4
     });
 
     await transporter.sendMail({
+
       from: process.env.EMAIL_USER,
+
       to: email,
+
       subject: "Interview Prep AI OTP",
 
       html: `
@@ -33,14 +34,14 @@ const sendOtpEmail = async (email, otp) => {
         <p>This OTP expires in 5 minutes.</p>
       `,
     });
-    console.log("BEFORE EMAIL SEND");
-    console.log("OTP EMAIL SENT");
-    console.log("AFTER EMAIL SEND");
+
+    console.log("EMAIL SENT SUCCESS");
+
   } catch (error) {
 
     console.log("EMAIL ERROR:");
     console.log(error);
-    
+
     throw error;
   }
 };
