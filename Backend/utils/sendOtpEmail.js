d2b3f4bb46c55e4cefa9c7288e2919c42364cdab
Reshoo-18 <reshooranjan7@gmail.com>
2026-05-11 14:18:06 +0530

    otps done

--- Backend/utils/sendOtpEmail.js
+++ Backend/utils/sendOtpEmail.js
@@ -16,23 +16,20 @@ const sendOtpEmail = async (email, otp) => {
       process.env.EMAIL_PASS
     );
 
-    const transporter =
-      nodemailer.createTransport({
+   const transporter = nodemailer.createTransport({
+  service: "gmail",
 
-        service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
 
-        auth: {
-          user:
-            process.env.EMAIL_USER,
+  family: 4,
 
-          pass:
-            process.env.EMAIL_PASS,
-        },
-
-        connectionTimeout: 10000,
-        greetingTimeout: 10000,
-        socketTimeout: 10000,
-      });
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
     console.log("STEP B");
 
