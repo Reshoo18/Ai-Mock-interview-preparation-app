b78eecf68255f2e42672b0c049359ee420936e5e
Reshoo-18 <reshooranjan7@gmail.com>
2026-05-11 16:52:45 +0530

    fixing

--- Backend/utils/sendOtpEmail.js
+++ Backend/utils/sendOtpEmail.js
@@ -1,13 +1,17 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+
+  tls: {
+    rejectUnauthorized: false,
+  },
+
+  connectionTimeout: 10000,
 });
 
 const sendOtpEmail = async (email, otp) => {
